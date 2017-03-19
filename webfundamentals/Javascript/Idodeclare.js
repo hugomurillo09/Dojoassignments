7adba249b06a7b56d38b30315741f3b33703d4cd
Hugo Murillo <murillohugo@yahoo.com>
2017-03-19 13:09:06 -0500

    adding I Do Declare assignment

---
+++ webfundamentals/Javascript/Idodeclare.js
@@ -0,0 +1,21 @@
+var num1 = -20
+var num2 = 20.3
+var num3 = 35
+var num4 = 40.2
+var string1 = "Hello World!"
+var string2 = "123456"
+var string3 = "Goodbye!"
+var boo1 = true
+var boo2 = false
+var undef = '';
+
+console.log("This is Number 1: ", num1);
+console.log("This is Number 2: ", num2);
+console.log("This is Number 3: ", num3);
+console.log("This is Number 4: ", num4);
+console.log("The first string: ", string1);
+console.log("The second string: ", string2);
+console.log("The third string: ", string3);
+console.log("My hair is brown - ", boo1);
+console.log("My eyes are blue - ", boo2)
+console.log(undef);
