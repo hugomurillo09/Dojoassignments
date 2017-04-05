d9c99cb34016082513d4187211c7e8ca8ca30d9f
Hugo Murillo <murillohugo@yahoo.com>
2017-04-04 19:27:06 -0500

    Comparing Arrays

---
+++ python/Comparing_arrays.py
@@ -0,0 +1,27 @@
+a = [1,2,5,6,2]
+b = [1,2,5,6,2]
+if a==b:
+   print "the list are the same"
+elif a != b:
+   print "the list are not the same"
+
+a = [1,2,4,6,5]
+b = [1,2,5,6,5,3]
+if a == b:
+	print "the list are the same"
+elif a != b:
+	print "the list are not the same"
+
+a = [1,2,5,6,5,16]
+b = [1,2,5,6,5]
+if a == b:
+	print "the list are the same"
+elif a != b:
+	print "the list are not the same"
+
+a = ['celery','carrots','bread','milk']
+b = ['celery','carrots','bread','cream']
+if a == b:
+	print "the list are the same"
+elif a != b:
+	print "the list are not the same"
