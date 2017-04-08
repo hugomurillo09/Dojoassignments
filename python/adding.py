9190e99af5f303f2bdeaccfd6b6b66549d5545cb
Hugo Murillo <murillohugo@yahoo.com>
2017-04-08 13:44:12 -0500

    adding assignment

---
+++ python/adding.py
@@ -0,0 +1,13 @@
+for count in range (1,1001,2):
+ print count
+
+for count in range (5,10000001,5):
+ print count
+
+my_number = [1,2,5,10,225]
+sum = 0
+for i in my_number:
+	sum += i
+	print sum
+
+	print sum/len(my_number)
