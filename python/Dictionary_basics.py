23e83ddeb2be3bc065e7573b836e8f976fbe8ad2
Hugo Murillo <murillohugo@yahoo.com>
2017-04-08 13:45:24 -0500

    Dictionary_basics

---
+++ python/Dictionary_basics.py
@@ -0,0 +1,6 @@
+dict = {'Name': 'Hugo', 'age': '25', 'birth place': 'USA', 'Favorite language': 'Python'}
+
+print "My Name is" + " " + dict['Name']
+print "My age is" + " " + dict['age']
+print "My country of birth is the" + " " + dict['birth place']
+print "My favorite language is" + " " + dict['Favorite language']
